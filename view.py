42f14711ebbf93b1c247ece5405fc9d437ecdc13
Pieter Marsman <pietermarsman@gmail.com>
2016-12-12 23:43:03 +0100

    Logging in with user password

--- view.py
+++ view.py
@@ -1,22 +1,36 @@
 import datetime
 
-import flask
 from flask import Flask, jsonify
+from flask import make_response
 from flask import redirect
-from flask_login import login_required, LoginManager, login_user, logout_user, \
-    current_user
+from flask_httpauth import HTTPBasicAuth
 
 from alarmclock import AlarmClock
-from user import User
+
+FILE_VIDEO = '/tmp/youtube_video.mp4'
+FILE_AUDIO = '/tmp/youtube_audio.mp3'
 
 context = ('/home/pieter/other/certificates/server.crt',
            '/home/pieter/other/certificates/server.key')
 
-login_manager = LoginManager()
-alarm = AlarmClock('top40')
+alarm = AlarmClock('top40', tmp_video_file=FILE_VIDEO,
+                   tmp_audio_file=FILE_AUDIO)
 
 app = Flask(__name__)
 app.secret_key = 'pietersecretkey'
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'pieter':
+        return 'wakemeup'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
 def get_state():
@@ -27,77 +41,53 @@ def get_state():
         'stop alarm': ['/stop'],
         'change alarm type': ['/alarm_type/<str:alarm_type>'],
         'remove alarm': ['/remove/<int:i>']}, 'state': alarm.jsonify(),
-        'user': {'logged_in': current_user.is_authenticated},
         'time': datetime.datetime.now()}
     return jsonify(json_state)
 
 
-@app.route('/login/<string:password>')
-def login(password):
-    if password == "wakemeup":
-        login_user(load_user("1"))
-
-        next = flask.request.args.get('next')
-        return flask.redirect(next or flask.url_for('index'))
-    return get_state()
-
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect('/')
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
-
-
 @app.route('/')
-@login_required
+@auth.login_required
 def index():
     return get_state()
 
 
 @app.route('/alarm/<string:day>/<int:hour>/<int:minute>')
 @app.route("/alarm/<int:hour>/<int:minute>")
-@login_required
+@auth.login_required
 def set_alarm(hour, minute, day=None):
     alarm.set_single_alarm('%.2d:%.2d' % (hour, minute), day)
-    return get_state()
+    return redirect("/", code=302)
 
 
 @app.route('/remove/<int:i>')
-@login_required
+@auth.login_required
 def remove_alarm(i):
     alarm.remove_single_alarm(i)
-    return get_state()
+    return redirect("/", code=302)
 
 
 @app.route("/snooze")
 @app.route("/snooze/<int:duration>")
-@login_required
+@auth.login_required
 def snooze(duration=5):
     alarm.snooze(duration)
-    return get_state()
+    return redirect("/", code=302)
 
 
 @app.route("/stop")
-@login_required
+@auth.login_required
 def stop_alarm():
     alarm.stop()
-    return get_state()
+    return redirect("/", code=302)
 
 
 @app.route('/alarm_type/<string:alarm_type>')
-@login_required
+@auth.login_required
 def change_alarm_type(alarm_type):
     alarm.alarm_type = alarm_type
-    return get_state()
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
     alarm.start()
-    login_manager.init_app(app)
     app.run(host='0.0.0.0', debug=True, ssl_context=context)
