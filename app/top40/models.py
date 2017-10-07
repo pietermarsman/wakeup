cf83acfaf14d6699a2fd57699b4f3151d8322fd2
Pieter Marsman <pietermarsman@gmail.com>
2017-10-07 15:17:27 +0200

    fix #11: model for storing top40 songs

--- app/top40/models.py
+++ app/top40/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Top40Song(models.Model):
+    title = models.CharField(max_length=512)
+    artist = models.CharField(max_length=512)
+    first_appeared = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('title', 'artist')
